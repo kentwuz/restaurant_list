cae4dfb91738afb3b1ff0c17444e05f161e9f3b4
0xKent <kentw8888@gmail.com>
2022-11-15 15:29:33 +0800

    feat: add index-page content and stylesheets

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //資料庫設定
 const mongoose = require('mongoose') //載入Mongoose
+const Restaurant = require('./models/restaurant')//載入restaurant model
 //非正式環境下使用dotenv
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -15,15 +16,19 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
 //express設定
 const express = require('express')
 const app = express()
-
 //設定template engine
 const { engine } = require('express-handlebars')
 app.engine('hbs', engine({ defaultLayout: "main", extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(express.static('public'))//使用靜態檔案
 
 app.get('/', (req, res) => {
-  res.render('index')
+  Restaurant.find() //取出資料庫中資料
+    .lean()
+    .then(restaurants => res.render('index', { restaurants })) //將相關資料傳給index樣板
+    .catch(error => console.log(error))
 })
+
 
 app.listen(3000, () => {
   console.log('App is running on https://localhost:3000')
