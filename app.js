2eba3205702df1bf0fc6219a748f2a7b11b23428
0xKent <kentw8888@gmail.com>
2022-11-15 10:34:39 +0800

    feat: add express-handlebars & basic layout

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 //資料庫設定
 const mongoose = require('mongoose') //載入Mongoose
-const db = mongoose.connection
-db.on('error',()=> {
-  console.log('MongoDB error')
-})
-db.once('open',()=> {
-  console.log('MongoDB connect!')
-})
-//express設定
-const express = require('express')
-const app = express()
 //非正式環境下使用dotenv
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('MongoDB error')
+})
+db.once('open', () => {
+  console.log('MongoDB connect!')
+})
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })//設定連線到MongoDB
+//express設定
+const express = require('express')
+const app = express()
 
+//設定template engine
+const { engine } = require('express-handlebars')
+app.engine('hbs', engine({ defaultLayout: "main", extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
+app.get('/', (req, res) => {
+  res.render('index')
+})
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })//設定連線到Mongoose DB
+app.listen(3000, () => {
+  console.log('App is running on https://localhost:3000')
+})
